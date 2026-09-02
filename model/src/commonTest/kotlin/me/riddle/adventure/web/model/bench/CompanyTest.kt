de2df8c7519b9b413cbfd16032781455aee4ffb9
R!dd13r <12781006+rdd13r@users.noreply.github.com>
2026-09-02 04:44:39 -0400

    Deslopifying MODEL
    Locked down to coverage tests.

--- model/src/commonTest/kotlin/me/riddle/adventure/web/model/bench/CompanyTest.kt
+++ model/src/commonTest/kotlin/me/riddle/adventure/web/model/bench/CompanyTest.kt
@@ -9,9 +9,16 @@ package me.riddle.adventure.web.model.bench
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.serialization.json.Json
-import me.riddle.adventure.web.model.bench.CorporateGroup.Companion.of
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import me.riddle.adventure.web.model.bench.CorporateDivision.Companion.DIVISIONS
+import me.riddle.adventure.web.model.bench.CorporateDivision.Companion.DIVISIONS_ALL
+import me.riddle.adventure.web.model.bench.CorporateGroup.Companion.GROUPS
+import me.riddle.adventure.web.model.bench.ProductTeam.Companion.TEAMS
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class CompanyTest {
 
@@ -21,23 +28,18 @@ class CompanyTest {
         val company by lazy {
             Company(
                 Dataset.SMOKE,
-                listOf(
+                divisions = listOf(
                     CorporateDivision.of(
-                        id = 0, corporateGroups = listOf(
-                            of(
-                                id = 0, productTeams = listOf(
+                        id = 0,
+                        corporateGroups = listOf(
+                            CorporateGroup.of(
+                                id = 0,
+                                productTeams = listOf(
                                     ProductTeam.of(
-                                        id = 0, teamMembers = listOf(
-                                            Person.of(index = 0)
-                                        )
-                                    )
-                                )
-                            )
-                        )
-                    )
-                ),
-            )
-        }
+                                        id = 0,
+                                        teamMembers = listOf(// @formatter:off
+                                            Person.of(index = 0))))))))) }
+        // @formatter:on
     }
 
     @Test
@@ -76,4 +78,59 @@ class CompanyTest {
                 .also { tLogger.trace { "Company is: $it" } }),
         message = "The whole tree survives the wire"
     )
+
+    @Test
+    fun `base divisions is omnipresent`() {
+        val (divId, division, groups) = CorporateDivision.of(id = 7, corporateGroups = listOf())
+        assertEquals(7, divId)
+        assertTrue(groups.isEmpty())
+        assertEquals("Marketing", division)
+
+        val (gId, group, teams) = CorporateGroup.of(id = 7, productTeams = listOf())
+        assertEquals(7, gId)
+        assertTrue(teams.isEmpty())
+        assertEquals("Networking 7", group)
+
+        val (tId, team, people) = ProductTeam.of(id = 7, teamMembers = listOf())
+        assertEquals(7, tId)
+        assertTrue(people.isEmpty())
+        assertEquals("Harbor 7", team)
+
+        val (id, firstName, lastName, jobTitle, location, phone) = Person.of(index = 711)
+        assertEquals(expected = 711, actual = id, message = "Must be a cop?")
+        assertEquals(expected = "James", actual = firstName, message = "Not Billy?")
+        assertEquals(expected = "Mendoza", actual = lastName, message = "Whole day")
+        assertEquals(expected = "UX Designer", actual = jobTitle, message = "Because it's pretty and doesn't pop the Owl!")
+        assertEquals(expected = "Remote", actual = location, message = "But of course")
+        assertEquals(expected = "(550) 557-6936", actual = phone, message = "Call! Who you get?")
+
+        tLogger.info { "Generators are always to be trusted, if Joe Random Hacker is." }
+
+        // @formatter:off
+        @Suppress("JSON_FORMAT_REDUNDANT")
+        val liferJamesOfHarborTeamNetworkingInMarketing = Json { prettyPrint = true }
+            .encodeToString<CorporateDivision>(
+                value = CorporateDivision.of(id = 7, corporateGroups = listOf(
+                    CorporateGroup.of(id = 7, productTeams = listOf(
+                        ProductTeam.of(id = 7, teamMembers = listOf(
+                            Person.of(index = 711))))))))
+        // @formatter:on
+
+        val james = Json.parseToJsonElement(liferJamesOfHarborTeamNetworkingInMarketing)
+
+        tLogger.info { "But if you're a hacker, you can't trust anyone." }
+        assertEquals("Marketing", james.jsonObject["division"]?.jsonPrimitive?.content ?: "Kitchen", "Not kitchen.")
+        assertEquals(
+            "Networking 7", james.jsonObject["groups"]
+                ?.jsonArray?.get(0)?.jsonObject?.get("group")?.jsonPrimitive?.content ?: "Goths", "Not Goths."
+        )
+        val jamesDivision = Json.decodeFromString<CorporateDivision>(james.toString())
+        assertEquals("James", jamesDivision.groups[0].teams[0].people[0].firstName, "Can't trust James.")
+
+        assertEquals(36, DIVISIONS_ALL.size, "Because Acme is big.")
+        assertEquals(13, DIVISIONS.size, "But not all count.")
+        assertEquals(20, GROUPS.size, "And there are groups.")
+        assertEquals(24, TEAMS.size, "And even teams.")
+    }
+
 }
